6ee08e2f883d7c41c68e97d9070a97b97523955c
BaptLeVrai <baptiste.drd53@gmail.com>
2023-04-18 14:59:29 +0200

    Create secrets.h.ino

---
+++ secrets.h/secrets.h.ino
@@ -0,0 +1,10 @@
+void setup() {
+  // put your setup code here, to run once:
+const char* ssid='votressid';
+const char* password= 'votremdp';
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
